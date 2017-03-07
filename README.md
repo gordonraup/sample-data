dda7827e2af18bfccbac72e9173bf4beea2b3d04
Dave Carlson <dcarlson@xmlmodeling.com>
2017-03-06 19:24:15 -0700

    Add veteran examples

---
+++ README.md
@@ -0,0 +1,47 @@
+# Sample FHIR CarePlan Resources
+
+Sample 🔥FHIR CarePlan and related resources for chronic care scenarios.
+
+Because FHIR is still evolving there may be different versions of the resources:
+
+- the `master` branch contains **STU-3** (v`1.8.0`) resources
+- the `develop` branch contains (nothing at this time)
+
+Diabetes Care Plan Scenarios
+===================
+These FHIR care plan scenarios were originally created to support the [Care Plan Track of HL7 FHIR Connectathon 14][track], held on Jan 14-15, 2017. The scenario scope and contents are expected to evolve and grow based on both clinical and technical experience.
+
+The sample resources are based on a storyboard for chronic conditions that illustrates the clinical documentation used and created by care providers, and communication flow between a patient and care team members involved in his/her long term care. The chronic condition selected for the first storyboard is [Type II Diabetes Mellitus (Type II DM)](./Diabetes/Care Plan Storyboard - Diabetes Mellitus.pdf).
+
+For the Type II Diabetes scenario, the FHIR resource files are divided into four folders:
+
+- Patients: Patients, RelatedPerson, and other clinical resources
+- Providers: Organizations, Practitioners, other related resources
+- PCP: healthcare provider organization for patient’s Primary Care Provider (PCP)
+- Dietitian: healthcare provider organization for a specialist referral
+- Podiatry: healthcare provider organization for a specialist referral
+
+Initially, all of these provider organization resources are loaded into a single FHIR server, but we expect to distribute the clinical content so that each provider organization is supported by a distinct FHIR server.
+
+[track]: http://wiki.hl7.org/index.php?title=201701_Care_Plan
+
+Veteran Clinical Scenarios
+===================
+Description TBD
+
+
+Loading these files into your FHIR server
+-----------------------------------------
+Each file contains a single FHIR resource.  A shell script is included that loads all resources into your FHIR server:
+
+`$. load-all http://fhir3.healthintersections.com.au/open > log.txt`
+
+or `$. load-all http://localhost:8080/fhir > log.txt`
+
+License
+-------
+
+This work is [Apache 2](./LICENSE.txt) licensed.
+FHIR® is the registered trademark of [HL7][hl7] and is used with the permission of HL7.
+
+[hl7]: http://hl7.org/
---
+++ Veterans/load-all.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Load all care plan resources into a single server
+
+# arg1 = FHIR server endpoint URL
+# . load-all http://fhir3.healthintersections.com.au/open
+
+# Providers
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-francis
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-pamela
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-joann
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-joe
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-amy
+
+. ../load-resource.sh $1 Organization Providers va-org
+. ../load-resource.sh $1 Organization Providers va-org-vha
+. ../load-resource.sh $1 Organization Providers va-org-visn6
+. ../load-resource.sh $1 Organization Providers va-org-visn6-vamc1
+
+. ../load-resource.sh $1 Organization Providers va-org-visn6-cboc1
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs1
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs2
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs3
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs4
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs5
+. ../load-resource.sh $1 HealthcareService Providers va-org-visn6-cboc1-hs6
+
+. ../load-resource.sh $1 Organization Providers va-org-visn7
+. ../load-resource.sh $1 Organization Providers va-org-visn7-vamc1
+
+# Patients
+. ../load-resource.sh $1 Patient Patients va-pat-meghan
+. ../load-resource.sh $1 Patient Patients va-pat-dan
+. ../load-resource.sh $1 Patient Patients va-pat-byron
+
+. ../load-resource.sh $1 Condition Patients va-cond-meghan-backpain
+. ../load-resource.sh $1 Condition Patients va-cond-meghan-depression
+. ../load-resource.sh $1 Condition Patients va-cond-meghan-headache
+. ../load-resource.sh $1 Condition Patients va-cond-meghan-ptsd
+. ../load-resource.sh $1 Condition Patients va-cond-meghan-weight
+
+. ../load-resource.sh $1 Condition Patients va-cond-dan-depression
+. ../load-resource.sh $1 Condition Patients va-cond-dan-ptsd
+. ../load-resource.sh $1 Condition Patients va-cond-dan-hypertension
+. ../load-resource.sh $1 Condition Patients va-cond-dan-dm
+
+. ../load-resource.sh $1 Condition Patients va-cond-byron-depression
+. ../load-resource.sh $1 Condition Patients va-cond-byron-ptsd
+. ../load-resource.sh $1 Condition Patients va-cond-byron-alcoholism
+
+# Patient Goals
+. ../load-resource.sh $1 Goal Patients va-goal-meghan-1
+. ../load-resource.sh $1 Goal Patients va-goal-dan-1
+. ../load-resource.sh $1 Goal Patients va-goal-byron-1
+
+# Patient Medications
+. ../load-resource.sh $1 MedicationRequest Patients va-med-meghan-prozac
+
+. ../load-resource.sh $1 MedicationRequest Patients va-med-dan-prozac
+. ../load-resource.sh $1 MedicationRequest Patients va-med-dan-metformin
+. ../load-resource.sh $1 MedicationRequest Patients va-med-dan-lisinopril
+
+#. ../load-resource.sh $1 DiagnosticRequest Patients CarePlan-DiagnosticRequest-1
+
+#. ../load-resource.sh $1 ReferralRequest Patients CarePlan-ReferralRequest-1
+
+# CareTeam and CarePlan
+. ../load-resource.sh $1 CareTeam Providers va-team-visn6-cboc1-pc
+. ../load-resource.sh $1 CarePlan Patients va-plan-meghan-pc
