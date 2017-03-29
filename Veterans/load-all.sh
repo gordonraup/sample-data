ab3e6ff7ccb36e9292680922ac7df783a100f4bb
Dave Carlson <dcarlson@xmlmodeling.com>
2017-03-29 06:44:36 -0600

    Add care plans for Byron

--- Veterans/load-all.sh
+++ Veterans/load-all.sh
@@ -12,6 +12,7 @@
 . ../load-resource.sh $1 Practitioner Providers va-prac-visn6-joann
 . ../load-resource.sh $1 Practitioner Providers va-prac-visn6-joe
 . ../load-resource.sh $1 Practitioner Providers va-prac-visn6-amy
+. ../load-resource.sh $1 Practitioner Providers va-prac-visn6-beach
 
 . ../load-resource.sh $1 Organization Providers va-org
 . ../load-resource.sh $1 Organization Providers va-org-vha
@@ -52,14 +53,21 @@
 
 # Patient Goals
 . ../load-resource.sh $1 Goal Patients va-goal-meghan-1
+. ../load-resource.sh $1 Goal Patients va-goal-meghan-weight
+. ../load-resource.sh $1 Goal Patients va-goal-meghan-child
 . ../load-resource.sh $1 Goal Patients va-goal-dan-1
+
 . ../load-resource.sh $1 Goal Patients va-goal-byron-1
+. ../load-resource.sh $1 Goal Patients va-goal-byron-crowds
+. ../load-resource.sh $1 Goal Patients va-goal-byron-sleep
 
 # Patient Medications
 . ../load-resource.sh $1 MedicationRequest Patients va-med-meghan-prozac
 . ../load-resource.sh $1 MedicationRequest Patients va-med-meghan-aptiom
 . ../load-resource.sh $1 MedicationRequest Patients va-med-meghan-vitamin-d
 . ../load-resource.sh $1 MedicationRequest Patients va-med-meghan-folic-acid
+
+. ../load-resource.sh $1 MedicationRequest Patients va-med-byron-prozac
 
 . ../load-resource.sh $1 MedicationRequest Patients va-med-dan-prozac
 . ../load-resource.sh $1 MedicationRequest Patients va-med-dan-metformin
@@ -70,8 +78,12 @@
 #. ../load-resource.sh $1 ReferralRequest Patients CarePlan-ReferralRequest-1
 
 # CareTeam and CarePlan
-. ../load-resource.sh $1 CareTeam Providers va-team-visn6-cboc1-pc
+. ../load-resource.sh $1 CareTeam Providers va-team-visn6-vamc1-pc
+. ../load-resource.sh $1 CareTeam Providers va-team-visn6-cboc1-mental-health
 . ../load-resource.sh $1 CarePlan Patients va-plan-meghan-pc
 
 . ../load-resource.sh $1 CareTeam Providers va-team-visn6-cboc1-mcc
 . ../load-resource.sh $1 CarePlan Patients va-plan-meghan-prenatal
+
+. ../load-resource.sh $1 CarePlan Patients va-plan-byron-pc
+. ../load-resource.sh $1 CarePlan Patients va-plan-byron-mental
